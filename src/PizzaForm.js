1aa06e31ae987dd46ba1d8c6680f110436ebc70f
Jacob Lang <jacoblang72@comcast.net>
2023-08-10 12:19:20 -0400

    wip

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -8,26 +8,26 @@ import * as yup from "yup";
 
 
 export default function Pizza(props) {
-    const {disabled,formData,change,submit,formErrors} = props;
+    const { disabled, formData, change, submit, formErrors } = props;
     return (
         <div>
-            <form onSubmit = {submit} id = "pizza-form">
+            <form onSubmit={submit} id="pizza-form">
                 <label htmlFor="name-input">Name : </label>
-                <input onChange={change} value = {formData.name}
-                 id = "name-input" name = "name" 
-                 type = "text" placeholder="name"
-                 />
-
-                 <label htmlFor="size-dropdown">Select Pizza Size</label>
+                <input onChange={change} value={formData.name}
+                    id="name-input" name="name"
+                    type="text" placeholder="name"
+                />
+                {formErrors.name && <p style={{ color: "red" }}>{formErrors.name}</p>}
+                
+                {/* <label htmlFor="size-dropdown">Select Pizza Size</label>
                  <select value = {formData.size} onChange={change} id = "size-dropdown">
                     <option value = "">--Select One--</option>
                     <option value = "small">Small</option>
                     <option value = "medium">Medium</option>
                     <option value = "large">Large</option>
+                 </select> */}
 
-                 </select>
-                 {formErrors.name && <p style = {{color : "red"}}>{formErrors.name}</p>}
-                 <input disabled = {disabled} type = "submit" id = "order-button"/>
+                <input disabled={disabled} type="submit" id="order-button" />
             </form>
         </div>
     )
