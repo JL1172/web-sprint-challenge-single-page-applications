cbd74071255a6b2e5d97785480e31eca722e6ff0
Jacob Lang <jacoblang72@comcast.net>
2023-08-10 11:57:22 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -4,18 +4,51 @@ import { Link, Route, Routes } from "react-router-dom"
 import styled from "styled-components";
 import Home from "./Home";
 import PizzaForm from "./PizzaForm";
+import * as yup from "yup";
 
 const StyledDiv = styled.div`
 
 `
+const schema = yup.object().shape({
+  name : yup.string().min(2,"name must be at least 2 characters")
+})
 
 const App = () => {
+  //!slices of state
+  const [formData,setFormData] = useState({name : "", size : "", topping1 : false, topping2 : false, special : ""})
+  const [formErrors,setFormErrors] = useState({name : "", size : "", topping1 : "", topping2 : "", special : ""})
+  const [disabled, setDisabled] = useState(true)
+  //!slices of state
+  //!handlers
+  const formValidation = (name,value) => {
+      yup.reach(schema,name).validate(value)
+      .then(()=> setFormErrors({...formErrors, [name] : ""}))
+      .catch(err=> setFormErrors({...formErrors, [name] : err.errors[0]}))
+  }
+  const change = evt => {
+    const {name,value, checked} = evt.target;
+    const valueToUse = evt.target.type === "checkbox" ? checked : value;
+    formValidation(name,valueToUse);
+    setFormData({...formData, [name] : valueToUse}); 
+  }
+  const submit = evt => {
+    evt.preventDefault();
+  }
+  //!handlers
+  //!useEffect
+  useEffect(()=> {
+    schema.isValid(formData).then(valid=> setDisabled(!valid))
+  },[formData])
+  //!useEffect
   return (
     <>
-      <Link to = "/">Home</Link>
+      <Link  id = "order-pizza" to = "/">Home</Link>
+      <Link to = "pizza">Order Now</Link>
       <Routes>
         <Route path = "/" element = {<Home />}/>
-        <Route path = "pizza" element = {<PizzaForm />}/>
+        <Route path = "pizza" element = {<PizzaForm change = {change} 
+        disabled ={disabled} formData = {formData} submit = {submit}
+        />}/>
       </Routes>
     </>
   );
--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -3,13 +3,21 @@ import { useState, useEffect } from "react";
 import { Link, Route, Routes } from "react-router-dom"
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import * as yup from "yup";
+
+
 
 export default function Pizza(props) {
+    const {disabled,formData,change,submit} = props;
     return (
         <div>
-            <form>
-                <label htmlFor="name">Name : </label>
-                <input id = "name" name = "name" type = "text" placeholder="name"/>
+            <form onSubmit = {submit} id = "pizza-form">
+                <label htmlFor="name-input">Name : </label>
+                <input onChange={change} value = {formData.name}
+                 id = "name-input" name-input = "name-input" 
+                 type = "text" placeholder="name"
+                 />
+                 <input type = "submit" id = "order-button"/>
             </form>
         </div>
     )
