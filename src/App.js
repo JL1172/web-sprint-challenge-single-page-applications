dccf7129fc39a39a793fe876a7137c80a0c47e3f
Jacob Lang <jacoblang72@comcast.net>
2023-08-11 01:01:17 -0400

    didmore

--- src/App.js
+++ src/App.js
@@ -19,35 +19,40 @@ img {
 }
 `
 const schema = yup.object().shape({
-  name : yup.string().required("Name is required").min(2,"name must be at least 2 characters"),
-  size : yup.string().oneOf(["small","medium","large"],"Must pick a size"),
-  topping1 : yup.boolean(),
-  topping2 : yup.boolean(),
-  topping3 : yup.boolean(),
-  topping4 : yup.boolean(),
-  special : yup.string(),
+  name: yup.string().required("Name is required").min(2, "name must be at least 2 characters"),
+  size: yup.string().oneOf(["small", "medium", "large"], "Must pick a size"),
+  topping1: yup.boolean(),
+  topping2: yup.boolean(),
+  topping3: yup.boolean(),
+  topping4: yup.boolean(),
+  special: yup.string(),
 });
-const initialValue = {name : "", size : "", topping1 : false, topping2 : false,
-topping3 : false,topping4 : false, special : ""}
+const initialValue = {
+  name: "", size: "", topping1: false, topping2: false,
+  topping3: false, topping4: false, special: ""
+}
 function App() {
   //!slices of state
-  const [formData,setFormData] = useState(initialValue);
-  const [formErrors,setFormErrors] = useState({name : "", size : ""});
+  const [formData, setFormData] = useState(initialValue);
+  const [formErrors, setFormErrors] = useState({ name: "", size: "" });
   const [disabled, setDisabled] = useState(true);
-  const [imgUrl,setImgUrl] = useState('');
-  const [orderInfo,setOrderInfo] = useState([]);
+  const [imgUrl, setImgUrl] = useState('');
+  const [orderInfo, setOrderInfo] = useState([]);
+  const [randomNum, setRandomNum] = useState("");
+  const [collapse,setCollapse] = useState(false)
+
   //!slices of state
   //!handlers
-  const formValidation = (name,value) => {
-      yup.reach(schema,name).validate(value)
-      .then(()=> setFormErrors({...formErrors, [name] : ""}))
-      .catch(err=> setFormErrors({...formErrors, [name] : err.errors[0]}))
+  const formValidation = (name, value) => {
+    yup.reach(schema, name).validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: "" }))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
   }
   const change = evt => {
-    const {name,value,type,checked} = evt.target;
+    const { name, value, type, checked } = evt.target;
     const valueToUse = type === "checkbox" ? checked : value;
-    formValidation(name,valueToUse);
-    setFormData({...formData, [name] : valueToUse}); 
+    formValidation(name, valueToUse);
+    setFormData({ ...formData, [name]: valueToUse });
   }
   // async function postData(newOrder) {
   //   const res = await axios.post(" https://reqres.in/api/users",newOrder)
@@ -59,56 +64,75 @@ function App() {
   //   }
   // }
   const navigate2 = useNavigate();
-    const nav1 = () => {
-        navigate2("pizza/order-detail")
-    }
+  const nav1 = () => {
+    navigate2("pizza/order-detail")
+  }
+  const confirmationNumber = () => {
+    setRandomNum(randomNum => randomNum = ((Math.floor(Math.random() * 10000000)) * (Math.floor(Math.random() * 10000000))))
+  }
   const submit = evt => {
     evt.preventDefault();
+    let array = [];
+    if (formData.topping1) array.push("pepperoni")
+    if (formData.topping2) array.push("sausage")
+    if (formData.topping3) array.push("bacon")
+    if (formData.topping4) array.push("mushrooms")
     const newOrder = {
-      name : formData.name,
-      size : formData.size,
-      topping1 : formData.topping1,
-      topping2 : formData.topping2,
-      topping3 : formData.topping3,
-      topping4 : formData.topping4,
-      special : formData.special,
+      name: formData.name.trim(),
+      size: formData.size,
+      toppings : array,
+      special: formData.special.trim(),
     }
-    axios.post(" https://reqres.in/api/users",newOrder)
-    .then((res)=> {
-      console.log(res)
-      setOrderInfo([res.data,...orderInfo])
-      nav1();
-    })
-    .catch(err=> {
-      console.error(err)
-    })
+    axios.post(" https://reqres.in/api/users", newOrder)
+      .then((res) => {
+        setOrderInfo([res.data, ...orderInfo])
+        nav1();
+        confirmationNumber();
+      })
+      .catch(err => {
+        console.error(err)
+      })
+      .finally(()=> {
+        setFormData(initialValue)
+      })
+  }
+  const collapseOrExpand = (evt) => {
+    setCollapse(!collapse);
+  
   }
   //!handlers
   //!useEffect
-  useEffect(()=> {
-    schema.isValid(formData).then(valid=> setDisabled(!valid))
-  },[formData])
-  useEffect(()=> {
-    setImgUrl(imgUrl => imgUrl = pizzas[Math.floor(Math.random()*4)].src);
-    },[])
+  useEffect(() => {
+    schema.isValid(formData).then(valid => setDisabled(!valid))
+  }, [formData])
+  useEffect(() => {
+    setImgUrl(imgUrl => imgUrl = pizzas[Math.floor(Math.random() * 4)].src);
+  }, [])
   //!useEffect
   const navigate = useNavigate();
   const nav = evt => {
-      navigate("/pizza")
+    navigate("/pizza")
   }
   return (
     <>
-    <StyledDiv>
-          <Link to = "/"><img src = "https://www.svgrepo.com/show/347172/menu-4.svg"/></Link>
-            <h1>BloomEats</h1>
-        </StyledDiv>
+      <StyledDiv>
+        <Link to="/"><img src="https://www.svgrepo.com/show/347172/menu-4.svg" /></Link>
+        <h1>BloomEats</h1>
+      </StyledDiv>
       <Routes>
-        <Route path = "pizza/*" element = {<PizzaForm change = {change} 
-        disabled ={disabled} formData = {formData} submit = {submit}
-        formErrors = {formErrors}
-        />}/>
-        <Route path ="pizza/order-detail" element = {<OrderDetails orderInfo = {orderInfo} />} />
-        <Route path = "/" element = {<Home imgUrl = {imgUrl} nav = {nav}/>}/>
+        <Route path="pizza/*" element={<PizzaForm change={change}
+          disabled={disabled} formData={formData} submit={submit}
+          formErrors={formErrors}
+        />} />
+        <Route path="pizza/order-detail" element=
+          {orderInfo.map((n, i) => {
+            return <OrderDetails key={i} orderInfo={n} randomNum={randomNum}
+             collapseOrExpand = {collapseOrExpand} collapse = {collapse}
+            />
+          })} />
+
+
+        <Route path="/" element={<Home imgUrl={imgUrl} nav={nav} />} />
       </Routes>
     </>
   )
--- src/OrderDetails.js
+++ src/OrderDetails.js
@@ -4,12 +4,61 @@ import { Link, Route, Routes } from "react-router-dom"
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import * as yup from "yup";
+import {Card,CardBody,CardText,CardTitle} from "reactstrap";
+
+const StyledDiv= styled.div`
+display : flex;
+justify-content : center;
+align-items : center;
+height : 100vh;
+
+div {
+    padding : 1rem;
+}
+
+img {
+    width : 30px;
+}
+`
+const Header = styled.div`
+display : flex;
+justify-content : space-between;
+font-weight : bold;
+`
+const img1 = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Collapse_Arrow_%2861412%29_-_The_Noun_Project.svg/2048px-Collapse_Arrow_%2861412%29_-_The_Noun_Project.svg.png";
+const img2 = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Collapse_Arrow_%2861412%29_-_The_Noun_Project.svg/2048px-Collapse_Arrow_%2861412%29_-_The_Noun_Project.svg.png";
+
 
 export default function OrderDetails(props) {
-    const {orderInfo} = props;
+    const {orderInfo,randomNum,collapse,collapseOrExpand} = props;
+    const result = collapse ? <img style = {{transition: ".3s"}} src = {img1} /> 
+    : <img style = {{transition: ".3s",transform : "rotate(180deg)"}} src = {img2}/>;
+    const navigate = useNavigate();
+    const nav = () => {
+        navigate("/")
+        window.location.reload();
+    }
     return (
-        <div>
-            order details 
-        </div>
+        <StyledDiv>
+        <Card>
+            <CardTitle style = {{textDecoration : "underline"}}>
+            <Header><>Order Details</><p onClick={nav}>Click here to leave this page</p></Header>
+            </CardTitle>
+            <CardBody>
+                <CardText>Thanks <span style = {{color : "royalblue"}}>{orderInfo.name}</span> for your order, below is the verification number</CardText>
+                <CardText style = {{color : "royalblue"}}>#{randomNum}{orderInfo.id}</CardText>
+                <div onClick={collapseOrExpand}><strong>Press to expand order details {result}</strong></div>
+                {collapse && <div style = {{transition : ".3s"}}>  <strong>Toppings :</strong>
+                    <div>
+                        {orderInfo.toppings && orderInfo.toppings.map((n,i)=> {
+                            return <li key = {i}>{n}</li>
+                        })}
+                    </div>
+                    <div><strong>Size :</strong> {orderInfo.size}</div>
+                    <div><strong>Special Request : </strong>{orderInfo.special ? orderInfo.special : "N/A"}</div>
+                </div>}
+            </CardBody>
+        </Card>
+        </StyledDiv>
     )
 }
